f7e1a267b52b2e1795221328abe87f2ff25f071b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-07 22:10:18 -0700

    Add files via upload

---
+++ Data/Char1Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcChar1.Models;
+
+namespace MvcChar1.Data
+{
+    public class MvcChar1Context : DbContext
+    {
+        public MvcChar1Context (DbContextOptions<MvcChar1Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Char1> Char1 { get; set; }
+    }
+}
