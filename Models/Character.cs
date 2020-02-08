22b2dab5b32b0b3b64b5d0bd8c141f7f4227c8de
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-07 21:55:02 -0700

    Add files via upload

---
+++ Models/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcChar1.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Race { get; set; }
+        public int Ac { get; set; }
+        public int Hp { get; set; }
+        public int Str { get; set; }
+        public int Con { get; set; }
+        public int Dex { get; set; }
+        public int Wis { get; set; }
+        public int Ine { get; set; }
+        public int Cha { get; set; }
+    }
+}
