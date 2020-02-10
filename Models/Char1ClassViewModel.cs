418df1b8b7614e3986d239802585cdf957f437eb
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-09 23:22:03 -0700

    Add files via upload

---
+++ Models/Char1ClassViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcChar1.Models
+{
+    public class Char1ClassViewModel
+    {
+        public List<Char1> Char1s { get; set; }
+        public SelectList Classes { get; set; }
+        public String Char1Class { get; set; }
+        public string SearchString { get; set; }
+    }
+}
